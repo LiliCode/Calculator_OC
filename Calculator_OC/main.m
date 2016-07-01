22f8afc95eb96f49614ce743ed80035f47e17181
lili <lili2011050508@163.com>
2016-07-01 17:51:50 +0800

    Initial Commit

---
+++ Calculator_OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calculator_OC
+//
+//  Created by 圈圈科技 on 16/7/1.
+//  Copyright © 2016年 圈圈科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
